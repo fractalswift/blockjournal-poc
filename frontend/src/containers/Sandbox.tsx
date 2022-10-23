783cb6bbba3d928776abc63105a00561b5861e5c
fractalswift <jb400@tutanota.com>
2022-10-23 17:44:26 +0100

    add express app for backend, WIP

--- frontend/src/containers/Sandbox.tsx
+++ frontend/src/containers/Sandbox.tsx
@@ -1,9 +1,14 @@
 import { uploadFileToIPFS } from '../lib';
 
 const Sandbox = () => {
+  const handleClick = () => {
+    fetch('http://localhost/3001/upload-output-to-ipfs');
+  };
+
   return (
     <>
       <button onClick={() => uploadFileToIPFS('Just testing')}>Press me</button>
+      <button onClick={handleClick}>Test backend</button>
     </>
   );
 };
