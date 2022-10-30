f43d1f72a35a93eebfee20f2decb62906f5227f3
fractalswift <jb400@tutanota.com>
2022-10-30 12:47:53 +0000

    update readme

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -2,7 +2,10 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p>Please connect Metamask to navigate around this web app.</p>
+      <p>
+        Please connect Metamask to navigate around this web app. y You can
+        ignore this message if you are already connected.
+      </p>
     </div>
   );
 };
